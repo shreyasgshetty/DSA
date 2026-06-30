587caaac86bbeb03beec865fe1105b54627a6775
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-06-30 22:22:40 +0530

    Time: 1 ms (27.58%), Space: 43.5 MB (6.26%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,11 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        for(int i = 0; i < s.length(); i++){
-            String rotated = s.substring(i)+ s.substring(0,i);
-            if(rotated.equals(goal)){
-                return true;
-            }
-        }
-        return false;
+        String ans = s + s;
+        return ans.contains(goal);
     }
 }
