1933423f8c2545e621bc5a4efd4580eda6efd800
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-07-02 19:53:04 +0530

    Time: 543 ms (10.97%), Space: 46.4 MB (29.77%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -4,25 +4,19 @@ class Solution {
     public int beautySum(String s) {
         int n = s.length();
         int sum = 0;
-
-        // Loop through all substrings
         for (int i = 0; i < n; i++) {
             Map<Character, Integer> freq = new HashMap<>();
 
             for (int j = i; j < n; j++) {
-                // Increase character frequency
                 freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j), 0) + 1);
 
                 int maxi = Integer.MIN_VALUE;
                 int mini = Integer.MAX_VALUE;
 
-                // Calculate max and min frequency
                 for (int val : freq.values()) {
                     mini = Math.min(mini, val);
                     maxi = Math.max(maxi, val);
                 }
-
-                // Add to sum
                 sum += (maxi - mini);
             }
         }
