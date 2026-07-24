7c9beda3d78d970027d47a10f283552028af6d4d
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-07-24 08:36:22 +0530

    Time: 28 ms (88.7%), Space: 48 MB (63.83%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,31 +1,28 @@
 class Solution {
+
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length%groupSize!=0) return false;
 
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        if (hand.length % groupSize != 0)
+            return false;
 
-        for(int card: hand){
-            hm.put(card,hm.getOrDefault(card,0)+1);
-            pq.add(card);
-        }
+        Arrays.sort(hand);
 
-        while(!pq.isEmpty()){
-            while (!pq.isEmpty() && hm.get(pq.peek()) == 0) {
-                pq.poll();
-            }
+        HashMap<Integer, Integer> freq = new HashMap<>();
 
-            if (pq.isEmpty())
-                break;
+        for (int card : hand)
+            freq.put(card, freq.getOrDefault(card, 0) + 1);
 
-            int start = pq.peek();
+        for (int card : hand) {
 
-            for (int i = start; i < start + groupSize; i++) {
+            if (freq.get(card) == 0)
+                continue;
 
-                if (!hm.containsKey(i) || hm.get(i) == 0)
+            for (int i = card; i < card + groupSize; i++) {
+
+                if (!freq.containsKey(i) || freq.get(i) == 0)
                     return false;
 
-                hm.put(i, hm.get(i) - 1);
+                freq.put(i, freq.get(i) - 1);
             }
         }
 
