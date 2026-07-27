d797115ae1a54e7d2aa12bbd9d0e8a6a89c3bcca
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-07-27 11:15:46 +0530

    Time: 1 ms (99.73%), Space: 47.3 MB (57.67%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump = 0; int l = 0; int r = 0;
+        while(r<nums.length-1){
+            int f = 0;
+            for(int i = l; i <= r; i++){
+                f =  Math.max(f, i+nums[i]);
+            }
+            l=r+1;
+            r=f;
+            jump++;
+        }
+        return jump;
+    }
+}
