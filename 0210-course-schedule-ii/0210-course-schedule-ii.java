05c92ebd9e26a22643f92ac28d548d84c2bafef9
Shreyas G Shetty <shreyasgshetty.18@gmail.com>
2026-08-04 12:10:53 +0530

    Time: 7 ms (28.28%), Space: 47.9 MB (15.39%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : prerequisites){
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int i = 0; i < numCourses;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i]==0) queue.add(i);
+        }
+
+        List<Integer> topo = new ArrayList<>();
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            topo.add(node);
+
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    queue.offer(it);
+                }
+            }
+        }
+         int[] arr = new int[numCourses];
+
+        if(topo.size()==numCourses){    
+            for(int i = 0; i < numCourses; i++){
+                arr[i] = topo.get(i);
+            }
+        return arr;
+        }
+
+        return new int[]{};
+        
+    }
+}
